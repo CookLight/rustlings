5bf262f49989bc745b11d5c06b6e28bc0046e11a
Abelabc <2542503181@qq.com>
2024-12-27 20:37:00 +0800

    rd

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,7 +1,8 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
-
+    //let mut a: [i32; 100] = [0; 100];
+    let mut a = vec![0; 100]; // 'a' is mutable and can grow or shrink in size
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,7 +11,14 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
-
+fn calculate_price_of_apples(x:i32)->i32{
+    if x<=40{
+    x*2
+    }
+    else{
+        x
+    }
+}
 fn main() {
     // You can optionally experiment here.
 }
